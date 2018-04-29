bd3c21c1c5536bcaaa9e614e42212403aaf3b0a9
Florêncio Vieira Filho <florenciovieira@gmail.com>
2018-04-29 15:39:39 -0300

    Atualização

---
+++ consulta/src/br/com/consulta/view/PainelRegistro.java
@@ -0,0 +1,10 @@
+package br.com.consulta.view;
+
+import br.com.consulta.comp.PanelBorderLayout;
+
+public class PainelRegistro extends PanelBorderLayout {
+	private static final long serialVersionUID = 1L;
+
+	public PainelRegistro() {
+	}
+}
